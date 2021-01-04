5a3bc3bf011d29662ada697940b8f090707fa565
ZhanLiangUF <zliang@ufl.edu>
2021-01-04 11:20:57 -0500

    extracted lex to a function call

--- compiler.go
+++ compiler.go
@@ -5,9 +5,8 @@ import (
 	"strings"
 )
 
-func main() {
-	str := "mul 3 sub 2 sum 1 3 4"
-	var entries = strings.Split(str, " ")
+func lex(s string) []string {
+	entries := strings.Split(s, " ")
 	lex := make([]string, 0)
 	for _, e := range entries {
 		eTrim := strings.Trim(e, " ")
@@ -15,5 +14,15 @@ func main() {
 			lex = append(lex, eTrim)
 		}
 	}
+	return lex
+}
+
+func parse(t []string) {
+
+}
+
+func main() {
+	str := "mul 3 sub 2 sum 1 3 4"
+	lex := lex(str)
 	fmt.Println(lex)
 }
