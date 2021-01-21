c4b031698283d0271caf25da6200726d94cd72b3
ZhanLiangUF <zliang@ufl.edu>
2021-01-21 17:02:07 -0500

    set up general functions for transpiler

--- cmd/compile/transpiler.go
+++ cmd/compile/transpiler.go
@@ -8,6 +8,18 @@ var ostMap = map[string]string{
 }
 
 // takes in AST and produce go code
-func transpile(ast Node) {
+func transpile(ast Node, ast2 Num) {
+
+}
+
+func transpileNode(ast Node, ast2 Num) {
+
+}
+
+func transpileOp(ast Node) {
+
+}
+
+func transpileNum(ast2 Num) {
 
 }
