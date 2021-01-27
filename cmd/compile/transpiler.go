6243550af373e969524db1592498c0fa90f70340
ZhanLiangUF <zliang@ufl.edu>
2021-01-27 17:13:15 -0500

    add general map logic and joining TODO: Fix return types

--- cmd/compile/transpiler.go
+++ cmd/compile/transpiler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 var ostMap = map[string]string{
@@ -14,7 +15,9 @@ var ostMap = map[string]string{
 
 // takes in AST and produce go code
 func transpile(ast *Node, ast2 *Num) {
-
+	a, b := transpileNode(ast, ast2)
+	fmt.Println(a)
+	fmt.Println(b)
 }
 
 func transpileNode(ast *Node, ast2 *Num) (*Node, *Num) {
@@ -28,10 +31,20 @@ func transpileNode(ast *Node, ast2 *Num) (*Node, *Num) {
 func transpileOp(ast *Node) {
 	// use map and fmtsprintf to interpolate
 	// iterate through num/node
-	var arr []interface{}
+	var arr []string
 	for i, v := range ast.Exp {
-		arr = append(arr, transpileNode(v))
+		if ast.Type == "Node" {
+			v := v.(*Node)
+			a, b := transpileNode(v, nil)
+			// append the result
+			// arr = append(arr, transpileNode(v, nil))
+		} else {
+			v := v.(*Num)
+			a, b := transpileNode(nil, v)
+			// arr = append(arr, transpileNode(nil, v))
+		}
 	}
+	s := strings.Join(arr, ostMap[ast.Val])
 	fmt.Println(arr)
 }
 
